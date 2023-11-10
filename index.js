b96e56f1a772317a4bebd81caedec3e2f7f59936
leroygatlin <leroygatlin@gmail.com>
2023-11-10 13:07:11 -0800

    complete intro looping lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, events) {
+ let array = []  
+    for (let a = 0; a < names.length; a++){
+    array.push(`Thank you, ${names[a]}, for the wonderful ${events} gift!`)     
+    }
+return array
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--)
+    }
+}
